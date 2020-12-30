d178ea285a918158defbd9facf28203d4f7bb1ab
Murphy <sydneyporter74@yahoo.com>
2020-12-29 22:30:24 -0500

    components created

---
+++ src/components/Contact/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Contact() {
+
+    return(
+        <section className="contact">Contact me:</section>
+    );
+}
+
+export default Contact;
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Portfolio() {
+
+    return(
+        <section className="portfolio">
+            Applications
+        </section>
+    );
+}
+
+export default Portfolio;
